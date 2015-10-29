a298240a9fd401a8c43fb899cb868f7e24407eee
HalahRaadSalih <hala.raad.salih@gmail.com>
2015-10-29 10:20:29 -0700

    added server delivers sandwich to customer

--- sandwichShop.js
+++ sandwichShop.js
@@ -10,6 +10,7 @@ var Shop = function(name){
 var Customer = function (name){
 	this.name = name;
 	this.order = new Order(this);
+	this.lunch = [];
 }
 
 Customer.prototype.orderSandwich = function(sandwich, order) {
@@ -18,8 +19,11 @@ Customer.prototype.orderSandwich = function(sandwich, order) {
 	// add it to the order
 };
 
-Customer.prototype.getSandwich = function() {
+Customer.prototype.getSandwich = function(shop) {
 	// customer picks sanwich up from counter
+	for(var i ; i < this.counter.length; i++){
+		this.lunch.push(this.counter[i]);
+	}
 };
 
 var Server = function(name, shop){
@@ -28,14 +32,15 @@ var Server = function(name, shop){
 }
 
 
-Server.prototype.getOrder = function(customer) {
-	// look at customer order
-	//queues all sandwiches in the order
-	this.queueOrder(this.shop, customer.order);
-};
+// Server.prototype.getOrder = function(customer) {
+// 	// look at customer order
+// 	//queues all sandwiches in the order
+// 	this.queueOrder(this.shop, customer.order);
+// };
 
-Server.prototype.queueOrder = function(shop, order) {
+Server.prototype.queueOrder = function(customer) {
 	// puts the sandwich from the order into the shop queue
+	var order = customer.order;
 
 	for(var i = 0; i < order.sandwiches.length; i++){
 		this.shop.sandwichQueue.push(order.sandwiches[i]);
@@ -46,6 +51,7 @@ Server.prototype.queueOrder = function(shop, order) {
 
 Server.prototype.deliverOrder = function(shop, customer) {
 	// call the customer get sandwich method
+	customer.getSandwich(this.shop);
 };
 
 
@@ -56,7 +62,17 @@ var Artist = function(name){
 
 Artist.prototype.makeSandwich = function(shop) {
 	//create new sandwich
+
 	//put it on the counter
+	var sandwich = new Sandwich(sandwich.name);
+	this.shop.counter.push(sandwich);
+};
+
+Artist.prototype.processQueue = function() {
+	//
+	for (var i = 0; i < shop.sandwichQueue.length; i++){
+		this.makeSandwich(shop.sandwichQueue[i]);
+	}
 };
 
 var Order = function(customer){
@@ -71,18 +87,20 @@ var Sandwich = function(name){
 
 
 // created shop with employees
-
 var hipwich = new Shop('hipwich');
 var jessica = new Server('Jessica')
 var kelly = new Artist('Kelly');
 
 // open for business, create some customers and put them in line
-
 var liz = new Customer('Liz');
 liz.order.sandwiches.push(new Sandwich('The Cosmopolitan'));
 
+//get in line
 hipwich.line.push(liz);
+// server takes her order
+jessica.queueOrder(hipwich.line.shift());
+//kelly make the sanswih
+kelly.processQueue();
+//jessica gives liz the sandwich
+jessica.deliverOrder();
 
-
-// server 
-jessica.getOrder(hipwich.line.shift());
